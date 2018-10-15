265942eedb4614cf8d6c6bd3ea1fc43c54af8f9f
Zack Fornaca <zfornaca@gmail.com>
2018-10-15 14:30:26 -0700

    oct 15 exercise pt 2

--- 2018-10.js
+++ 2018-10.js
@@ -57,3 +57,16 @@ var removeDuplicates = function(nums) {
   }
   return i + 1;
 };
+
+// Oct 15: Middle of the Linked List
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+var middleNode = function(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
