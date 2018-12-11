59e0eefbfaac1495c8df61c25ad6ddd76e54c25e
Zack Fornaca <zfornaca@gmail.com>
2018-12-10 16:21:42 -0800

    dec 10 exercises

--- 2018-12.js
+++ 2018-12.js
@@ -284,3 +284,243 @@ var deckRevealedIncreasing = function(deck) {
 
   return output;
 };
+
+// December 8
+// Sort array by parity
+// https://leetcode.com/problems/sort-array-by-parity/
+
+// version A: constant space, mutate input
+var sortArrayByParity = function(A) {
+  let left = 0;
+  let right = A.length - 1;
+
+  while (left < right) {
+    if (A[left] % 2 === 0) left++;
+    else if (A[right] % 2 === 1) right--;
+    else {
+      [A[left], A[right]] = [A[right], A[left]];
+    }
+  }
+
+  return A;
+};
+
+// version B: pure function, O(n) space
+var sortArrayByParity = function(A) {
+  let odds = [];
+  let evens = [];
+  for (num of A) {
+    if (num % 2 === 0) evens.push(num);
+    else odds.push(num);
+  }
+
+  return evens.concat(odds);
+};
+
+// December 8
+// Delete columns to make sorted
+// https://leetcode.com/problems/delete-columns-to-make-sorted/
+
+var minDeletionSize = function(A) {
+  let deletions = 0;
+
+  // for each column (letter position in word)
+  for (let i = 0; i < A[0].length; i++) {
+    // for each word
+    for (let j = 0; j < A.length - 1; j++) {
+      if (A[j][i] > A[j + 1][i]) {
+        deletions++;
+        break;
+      }
+    }
+  }
+
+  return deletions;
+};
+
+// December 8
+// Insert into a binary search tree
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+var insertIntoBST = function(root, val) {
+  if (root.val > val) {
+    // look left
+    if (root.left) insertIntoBST(root.left, val);
+    else root.left = new TreeNode(val);
+  } else {
+    // look right
+    if (root.right) insertIntoBST(root.right, val);
+    else root.right = new TreeNode(val);
+  }
+
+  return root;
+};
+
+// December 8
+// Flipping an image
+// https://leetcode.com/problems/flipping-an-image/
+
+var flipAndInvertImage = function(A) {
+  for (let row of A) {
+    let l = 0;
+    let r = row.length - 1;
+    while (l < r) {
+      [row[l], row[r]] = [1 - row[r], 1 - row[l]];
+      l++;
+      r--;
+    }
+    if (l === r) row[l] = 1 - row[l];
+  }
+  return A;
+};
+
+// faster
+var flipAndInvertImage = function(A) {
+  return A.map(row => row.reverse().map(num => (num === 1 ? 0 : 1)));
+};
+
+// December 8
+// Find and replace pattern
+// https://leetcode.com/problems/find-and-replace-pattern/
+
+var findAndReplacePattern = function(words, pattern) {
+  const letters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  abcWords = words.map(word => patternConvert(word, letters));
+  abcPattern = patternConvert(pattern, letters);
+
+  return words.filter((word, idx) => {
+    return abcPattern === abcWords[idx];
+  });
+};
+
+var patternConvert = function(word, letters) {
+  const mappedWord = [];
+  const usedLetters = new Map();
+  for (let ltr of word) {
+    if (!usedLetters.has(ltr)) {
+      usedLetters.set(ltr, letters[usedLetters.size]);
+    }
+    mappedWord.push(usedLetters.get(ltr));
+  }
+  return mappedWord.join('');
+};
+
+// December 9
+// Binary tree pruning
+// https://leetcode.com/problems/binary-tree-pruning/
+
+var pruneTree = function(root) {
+  if (!root) return null;
+  if (!pruneTree(root.left)) root.left = null;
+  if (!pruneTree(root.right)) root.right = null;
+  return root.val || root.left || root.right ? root : null;
+};
+
+// December 9
+// Merge two binary trees
+// https://leetcode.com/problems/merge-two-binary-trees/
+
+var mergeTrees = function(t1, t2) {
+  if (!t1 || !t2) return t1 || t2;
+  const t3 = new TreeNode(t1.val + t2.val);
+  t3.left = mergeTrees(t1.left, t2.left);
+  t3.right = mergeTrees(t1.right, t2.right);
+  return t3;
+};
+
+// December 10
+// DI string match
+// https://leetcode.com/problems/di-string-match/
+
+var diStringMatch = function(S) {
+  // const A = Array.from(Array(S.length+1).keys());
+  const output = [];
+  let lowest = 0;
+  let highest = S.length;
+  for (let dir of S) {
+    if (dir === 'I') output.push(lowest++);
+    if (dir === 'D') output.push(highest--);
+  }
+  output.push(highest);
+  return output;
+};
+
+// December 10
+// All paths from source to target
+// https://leetcode.com/problems/all-paths-from-source-to-target/
+
+var allPathsSourceTarget = function(graph) {
+  const validPaths = [];
+  const currentPath = [];
+
+  function dfs(node) {
+    currentPath.push(node);
+    if (node === graph.length - 1) validPaths.push(currentPath.slice(0));
+    else {
+      for (let next in graph[node]) {
+        dfs(graph[node][next]);
+      }
+    }
+    currentPath.pop();
+  }
+  dfs(0);
+  return validPaths;
+};
+
+// December 10
+// Self-dividing numbers
+// https://leetcode.com/problems/self-dividing-numbers/
+
+var selfDividingNumbers = function(left, right) {
+  const output = [];
+  for (num = left; num <= right; num++) {
+    let selfDividing = true;
+    for (let d of num.toString()) {
+      if (num % +d !== 0) {
+        selfDividing = false;
+        break;
+      }
+    }
+    if (selfDividing) output.push(num);
+  }
+  return output;
+};
+
+// December 10
+// Swap salary
+// https://leetcode.com/problems/swap-salary/
+
+/*
+UPDATE salary
+SET sex = CASE
+WHEN sex='m' THEN 'f'
+WHEN sex='f' THEN 'm'
+END;
+*/
